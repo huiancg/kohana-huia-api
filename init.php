be016f1970ebea17ed5ce6e2126c975d0bf6f006
Eduardo Pacheco <eduardo.pacheco@huia.com.br>
2015-11-09 19:32:56 -0200

    - Bug nas rotas.

--- init.php
+++ init.php
@@ -9,6 +9,27 @@ Route::set('api_user', 'api/user(/<action>)', array(
     'directory' => 'api'
   ));
 
+Route::set('api_model_actions', 'api(/<model>(/<model_action>(/<id>)))', array(
+    'id' => '\d+',
+  ))
+  ->filter(function($route, $params, $request) {
+    $api = 'Api_'.ORM::get_model_name(Arr::get($params, 'model'));
+    if ( ! class_exists($api))
+    {
+      return FALSE;
+    }
+    if ( ! method_exists($api, 'action_' . Arr::get($params, 'model_action', 'index')))
+    {
+      return FALSE;
+    }
+  })
+  ->defaults(array(
+    'controller'   => 'custom',
+    'action'       => 'index',
+    'model_action' => 'index',
+    'directory'    => 'api'
+  ));
+
 Route::set('api_actions', 'api(/<model>(/<id>))', array(
     'id' => '\d+',
   ))
@@ -16,24 +37,6 @@ Route::set('api_actions', 'api(/<model>(/<id>))', array(
     'controller' => 'app',
     'action'     => 'index',
     'directory' => 'api'
-  ));
-
-Route::set('api_model_actions', 'api(/<model>(/<model_action>(/<id>)))', array(
-    'id' => '\d+',
-  ))
-  ->filter(function($route, $params, $request) {
-    $api = 'Api_'.ORM::get_model_name(Arr::get($params, 'model'));
-    $exists = class_exists($api) AND method_exists($api, 'action_' . Arr::get($params, 'model_action', 'index'));
-    if ( ! $exists)
-    {
-      return FALSE;
-    }
-  })
-  ->defaults(array(
-    'controller'   => 'app',
-    'action'       => 'custom',
-    'model_action' => 'index',
-    'directory'    => 'api'
   ));
 
 Route::set('api', 'api(/<controller>(/<action>(/<id>)))', array(
